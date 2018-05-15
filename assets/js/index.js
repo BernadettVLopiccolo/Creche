5bc228faff9035b7facf1091753584cb527afc37
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2018-05-15 16:40:26 -0700

    added home page, with nav links

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,27 +5,52 @@ var lFollowX = 0,
     friction = 1 / 30;
 
 function moveBackground() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-  
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+    x += (lFollowX - x) * friction;
+    y += (lFollowY - y) * friction;
 
-  $('.bg').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
+    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
 
-  window.requestAnimationFrame(moveBackground);
+    $('.bg').css({
+        '-webit-transform': translate,
+        '-moz-transform': translate,
+        'transform': translate
+    });
+
+    window.requestAnimationFrame(moveBackground);
 }
 
 $(window).on('mousemove click', function(e) {
 
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
+    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+    lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+    lFollowY = (10 * lMouseY) / 100;
 
 });
 
 moveBackground();
+
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+        console.log("works");
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+                console.log("scrolled");
+            });
+        } // End if
+    });
+});
